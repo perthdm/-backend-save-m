b6cc17846fc293957de561e8d495b268ff767a3d
jakgrit <jakgrit_frame@hotmail.com>
2022-11-19 19:44:15 +0700

    body parser

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import bodyParser = require("body-parser");
 
 dotenv.config();
 
@@ -9,6 +10,7 @@ const port = process.env.PORT;
 
 app.use(cors());
 app.use(cors<Request>());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/get-user", (req: Request, res: Response) => {
   res.json({
